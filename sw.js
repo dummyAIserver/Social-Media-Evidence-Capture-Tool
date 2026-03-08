1d87a834cf9b9a6fb50259ed6bcb735e1d04d33b
dummyAIserver <dummypm753@gmail.com>
2026-03-08 19:36:05 +0530

    ...

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ const urlsToCache = [
   './',
   './seo.html',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
